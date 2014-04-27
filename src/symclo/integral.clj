cf9ccf7f02ce7da751ccef6013170d93e31c25f8
Avinash Malik <avinash.malik@auckland.ac.nz>
2014-04-27 17:15:22 +1200

    Started adding integration

--- src/symclo/integral.clj
+++ src/symclo/integral.clj
@@ -4,8 +4,44 @@
 (use 'clojure.math.numeric-tower)
 (use 'clojure.tools.trace)
 (require '[symclo.core :as simp])
+(require '[symclo.expand :as expand])
 (require '[symclo.trig :as trig])
+(require '[symclo.derivative :as deriv])
 (require '[symclo.util :as util])
 
-(defn- third [x] (first (nnext x)))
+(def third (comp first nnext))
 
+;;; TODO: fill in all the functions.
+(declare trial-sub)
+(declare linear-props)
+(declare integral-table)
+(declare substitution-method)
+(declare substitute)
+
+(defn- substitution-method [f x]
+  (let [P (trial-sub f)
+        FS (map #(if (and (util/free-of % x) (not= % x))
+                   (let [u (substitute (simp/simplify* (list '/ f (deriv/deriv* % x))) [% 'v])]
+                     (if (util/free-of u x)
+                       (substitute (integrate* u 'v) ['v %])))) P)]
+    (some (complement (partial = 'FAIL)) FS)))
+
+(defn integrate* [f x]
+  (let [F (integral-table f x)]
+    (if (= 'FAIL F) 
+      (let [F (linear-props f x)]
+        (if (= 'FAIL F)
+          (let [F (substitution-method f x)]
+            (if (= 'FAIL F)
+              (let [g (expand/expand* f)]
+                (if (not= g f) (integrate* g x)) F)
+              ;; else
+              F))
+          ;; else
+          F)) 
+      ;; else
+      F)))
+
+
+(defmacro integrate [arg sym]
+  `(simp/simplify* (integrate* (simp/simplify* '~arg) '~sym)))
